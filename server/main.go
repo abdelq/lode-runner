18247445d156d1b073497f9a896d9df2a361c404
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-09-10 19:43:24 -0400

    Rewriting the server using TCP sockets

--- server/main.go
+++ server/main.go
@@ -3,35 +3,25 @@ package main
 import (
 	"flag"
 	"log"
-	"net/http"
-
-	"github.com/google/uuid"
-	"github.com/gorilla/websocket"
+	"net"
 )
 
-var upgrader = websocket.Upgrader{}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	client := &Client{
-		id:     uuid.New(),
-		socket: conn,
-		send:   make(chan []byte),
-	}
-
-	go client.read()
-	go client.write()
-}
-
 func main() {
-	addr := flag.String("addr", ":3000", "server address")
+	addr := flag.String("addr", ":1337", "listener address")
 	flag.Parse()
 
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(*addr, nil))
+	ln, err := net.Listen("tcp", *addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer ln.Close()
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go newClient(conn)
+	}
 }
