ddd27eb57f4fd4af92bf95daf32fc65a6a540388
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-09-24 22:13:58 -0400

    Better Go client

--- clients/go/main.go
+++ clients/go/main.go
@@ -2,21 +2,55 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/json"
+	"flag"
+	"fmt"
 	"log"
 )
 
+const (
+	name = "316k"
+	room = "twado"
+)
+
+type message struct {
+	Event string
+	Data  json.RawMessage
+}
+
+func join(conn *tls.Conn) {
+	data := fmt.Sprintf(`{"name": "%s", "room": "%s"}`, name, room)
+
+	msg := message{
+		Event: "join",
+		Data:  json.RawMessage(data),
+	}
+
+	b, err := json.Marshal(&msg)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	conn.Write(b)
+	conn.Write([]byte("\n"))
+
+	for {
+	}
+}
+
 func main() {
-	conn, err := tls.Dial("tcp", ":443", &tls.Config{
-		InsecureSkipVerify: true,
-	})
+	// Flags
+	addr := flag.String("addr", ":443", "listener's network address")
+	flag.Parse()
+	log.SetFlags(log.Ltime)
+
+	// Connect to the server
+	conf := tls.Config{InsecureSkipVerify: true}
+	conn, err := tls.Dial("tcp", *addr, &conf)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 
-	_, err = conn.Read(make([]byte, 100))
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	join(conn)
 }
