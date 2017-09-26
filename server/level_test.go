500e285b72b5a160b3c2c4d0618bf1bb07f577fe
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-09-25 22:24:40 -0400

    Basic testing for 26x16 in each grid level

--- server/level_test.go
+++ server/level_test.go
@@ -7,6 +7,18 @@ func TestInit(t *testing.T) {
 	for i := 1; i <= 150; i++ {
 		if err := lvl.init(i); err != nil {
 			t.Error(err)
+			//continue
+		}
+
+		if len(lvl.grid) != 16 {
+			t.Fail()
+			//continue
+		}
+		for _, row := range lvl.grid {
+			if len(row) != 26 {
+				t.Fail()
+				//continue
+			}
 		}
 	}
 }
