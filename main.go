880e40a54706e495695455237983d16030cfffc6
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-11-16 00:10:55 -0500

    Enable TLS only when addr is set to 443

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"crypto/tls"
 	"flag"
 	"log"
+	"net"
 )
 
 func init() {
@@ -12,21 +13,29 @@ func init() {
 
 func main() {
 	// Command-line flags
-	addr := flag.String("addr", ":443", "network address")
+	addr := flag.String("addr", ":1337", "network address")
 	flag.Parse()
 
-	// Load public/private key pair
-	crt, err := tls.LoadX509KeyPair("server.crt", "server.key")
-	if err != nil {
-		log.Fatal(err)
-	}
-	conf := tls.Config{Certificates: []tls.Certificate{crt}}
-
 	// Listen on TCP
-	ln, err := tls.Listen("tcp", *addr, &conf)
+	var ln net.Listener
+	var err error
+	if _, port, _ := net.SplitHostPort(*addr); port == "443" {
+		// Load public/private key pair
+		crt, err := tls.LoadX509KeyPair("server.crt", "server.key")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		ln, err = tls.Listen("tcp", *addr, &tls.Config{
+			Certificates: []tls.Certificate{crt},
+		})
+	} else {
+		ln, err = net.Listen("tcp", *addr)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	log.Printf("Listening on %s %s", ln.Addr().Network(), ln.Addr())
 
 	defer ln.Close()
