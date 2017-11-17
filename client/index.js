bbe3017373bc09581f005c4fdd94dee752b75ea0
Nicolas Hurtubise <316k@legtux.org>
2017-11-17 15:15:53 -0500

    Work on TP2 files

--- client/index.js
+++ client/index.js
@@ -1,12 +1,22 @@
-var keypress = require('keypress');
-
 var {connect} = require('net');
 var tls = require('tls');
 var fs = require('fs');
-var {onkeypress, start, next} = require('./tp1.js');
-console.log(onkeypress, start, next);
 
-keypress(process.stdin);
+if (process.argv.indexOf('--clavier') > -1) {
+    var keypress = require('keypress');
+
+    var {onkeypress, start, next} = require('./clavier.js');
+
+    keypress(process.stdin);
+        process.stdin.on('keypress', function (ch, key) {
+        onkeypress(key);
+    });
+
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+} else {
+    var {start, next} = require('./tp2.js');
+}
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
@@ -22,7 +32,7 @@ function send(event, data) {
 // TODO Flag for port (process.argv)
 //const client = tls.connect(443, {ca: [ fs.readFileSync('server.crt') ]}, () => {
 //const client = tls.connect(1337, {}, () => {
-var client = tls.connect(443, {}, () => {
+var client = tls.connect("1337", "159.203.8.35", () => {
     console.log(`Connected to ${client.remoteAddress}:${client.remotePort}`);
     send("join", {"name": "runner", "room": "room"});
 });
@@ -47,10 +57,3 @@ client.on('data', (data) => {
         send(event, out);
     }
 });
-
-process.stdin.on('keypress', function (ch, key) {
-    onkeypress(key);
-});
-
-process.stdin.setRawMode(true);
-process.stdin.resume();
