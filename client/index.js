6b65d681f73111444e77b79036615784d07cbf17
Nicolas Hurtubise <316k@legtux.org>
2017-11-17 19:37:11 -0500

    Simplify the default client & boost html display

--- client/index.js
+++ client/index.js
@@ -1,22 +1,10 @@
+var room = "room";
+
 var {connect} = require('net');
 var tls = require('tls');
 var fs = require('fs');
 
-if (process.argv.indexOf('--clavier') > -1) {
-    var keypress = require('keypress');
-
-    var {onkeypress, start, next} = require('./clavier.js');
-
-    keypress(process.stdin);
-        process.stdin.on('keypress', function (ch, key) {
-        onkeypress(key);
-    });
-
-    process.stdin.setRawMode(true);
-    process.stdin.resume();
-} else {
-    var {start, next} = require('./tp2.js');
-}
+var {start, next} = require('./tp2.js');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
@@ -29,12 +17,9 @@ function send(event, data) {
     client.write(msg);
 }
 
-// TODO Flag for port (process.argv)
-//const client = tls.connect(443, {ca: [ fs.readFileSync('server.crt') ]}, () => {
-//const client = tls.connect(1337, {}, () => {
 var client = connect("1337", "159.203.8.35", () => {
     console.log(`Connected to ${client.remoteAddress}:${client.remotePort}`);
-    send("join", {"name": "runner", "room": "room"});
+    send("join", {"name": "runner", "room": room});
 });
 
 client.on('data', (data) => {
