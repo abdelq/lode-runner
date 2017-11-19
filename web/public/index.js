802c39e692ac0a38bb467d9036adad71d156cd3f
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-11-19 11:29:14 -0500

    Nicer web player

--- web/public/index.js
+++ web/public/index.js
@@ -1,12 +1,46 @@
-var socket = new WebSocket("ws://localhost:8080/ws"); // XXX
+/* Canvas */
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
 
-socket.addEventListener('open', function (event) {
+canvas.width  = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
+
+/* Socket */
+var socket = new WebSocket("ws://" + location.host + "/ws");
+
+socket.onopen = function () {
     socket.send(JSON.stringify({
         event: "join",
-        data: {name: "player", room: "room"}
+        data: {
+            name: "spectator",
+            room: location.hash.substr(1),
+            role: 42
+        }
     }));
-});
+}
 
-socket.addEventListener('message', function (event) {
-    console.log(event.data);
-});
+socket.onmessage = function (message) {
+    var msg = JSON.parse(message.data);
+    if (msg.event != "start" && msg.event != "next") {
+        console.log(msg);
+        return;
+    }
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    var tiles = msg.data.split("\n");
+    var tileHeight = canvas.height / tiles.length;
+    var tileWidth = canvas.width / tiles[0].length;
+
+    for (var i = 0; i < tiles.length; i++) {
+        for (var j = 0; j < tiles[i].length; j++) {
+            context.drawImage(
+                document.getElementById(tiles[i][j]),
+                j * tileWidth,
+                i * tileHeight,
+                tileWidth,
+                tileHeight
+            );
+        }
+    }
+}
