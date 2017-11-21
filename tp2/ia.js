64d32f8f41ed8d5f36d95b34276060f08ba00f5d
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-11-20 19:53:47 -0500

    Use transform to pipe delimited JSON to data

--- tp2/ia.js
+++ tp2/ia.js
@@ -1,4 +1,5 @@
 var {connect} = require('net');
+var stream = require('stream')
 var tls = require('tls');
 var fs = require('fs');
 
@@ -8,7 +9,6 @@ if(room == "") {
     console.error('Veuillez entrer une clé secrète dans tp2-ia.js');
     process.exit();
 }
-    
 
 var ip = "138.197.153.140";
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
@@ -26,7 +26,7 @@ var client = connect("1337", ip, () => {
     send("join", {"name": "runner", "room": room});
 });
 
-function oneJSON(str) {
+/*function oneJSON(str) {
     var count = 0;
 
     var start = str.indexOf('{');
@@ -42,11 +42,32 @@ function oneJSON(str) {
     }
 
     return str.slice(0, i);
+}*/
+
+var liner = new stream.Transform( { objectMode: true } )
+
+liner._transform = function (chunk, encoding, done) {
+     var data = chunk.toString()
+     if (this._lastLineData)
+        data = this._lastLineData + data
+
+     var lines = data.split('\n')
+     this._lastLineData = lines.splice(lines.length-1,1)[0]
+
+     lines.forEach(this.push.bind(this))
+     done()
 }
 
-client.on('data', (data) => {
+liner._flush = function (done) {
+     if (this._lastLineData)
+        this.push(this._lastLineData)
+     this._lastLineData = null
+     done()
+}
 
-    var json = {event: "none"};
+client.pipe(liner).on('data', (data) => {
+    var json = JSON.parse(data.toString('utf8').trim());
+    /*var json = {event: "none"};
 
     try {
         json = JSON.parse(data.toString('utf8').trim());
@@ -57,7 +78,7 @@ client.on('data', (data) => {
         console.error('JSON error : ', e, data.toString('utf8'));
 
         json = JSON.parse(oneJSON(data.toString('utf8').trim()));
-    }
+    }*/
 
     var events = {
         start: start,
