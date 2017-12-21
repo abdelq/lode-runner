703ae17d6f0b359890472fef79e6db9e51fcffcf
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-12-21 14:14:03 -0500

    JavaScript client

---
+++ clients/javascript/runner.js
@@ -0,0 +1,26 @@
+// Possible moves
+const moves = Object.freeze({
+    NONE: 0, UP: 1, LEFT: 2, DOWN: 3, RIGHT: 4
+});
+
+module.exports = class Runner {
+    constructor() {
+        this.name = 'name';
+        this.room = 'room';
+        this.level = 1;
+    }
+
+    start(grid) {
+        console.log(grid);
+    }
+
+    next(positions) {
+        console.log(positions); // XXX
+
+        // Random action (move or dig)
+        if (Math.random() < .5)
+            this.move(Math.floor(Math.random() * 4) + 1);
+        else
+            this.dig(Math.random() < .5 ? moves.LEFT : moves.RIGHT);
+    }
+}
